4e48da241ffd56256fe590ccae259f1c06cfc004
Nukepayload2 <nukepayload2@outlook.com>
2022-07-10 21:28:23 +0800

    Add assembly conflict log

--- XamlLanguageConverter/XamlLanguageConverter/ConvertCSharpToVB.vb
+++ XamlLanguageConverter/XamlLanguageConverter/ConvertCSharpToVB.vb
@@ -61,21 +61,28 @@ Namespace Tasks
 
                 Log.LogMessage($"Converting file '{inFile}'")
 
-                Dim convRequest As New ConvertRequest(False, False, Nothing, Nothing) With {
-                    .SourceCode = File.ReadAllText(inFile)
-                }
+                Try
+                    Dim convRequest As New ConvertRequest(False, False, Nothing, Nothing) With {
+                        .SourceCode = File.ReadAllText(inFile)
+                    }
 
-                Dim result = ConvertInputRequest(convRequest, vbOption,
-                                 (Aggregate con In consts Select con.Key Into ToList),
-                                 consts, references,
-                                 Sub(err) Log.LogWarning($"Error '{err.GetType.FullName}' while converting C# to VB: {err.Message}"),
-                                  Nothing)
+                    Dim result = ConvertInputRequest(convRequest, vbOption,
+                                     (Aggregate con In consts Select con.Key Into ToList),
+                                     consts, references,
+                                     Sub(err) Log.LogWarning($"Error '{err.GetType.FullName}' while converting C# to VB: {err.Message}"),
+                                      Nothing)
 
-                Dim converted = result.ConvertedCode
-                Dim outFile = String.Concat(inFile.AsSpan(0, inFile.Length - 2), "vb")
-                File.Delete(inFile)
-                deletedFiles.Add(New Microsoft.Build.Utilities.TaskItem(inFile))
-                generatedFiles.Add(New Microsoft.Build.Utilities.TaskItem(outFile))
+                    Dim converted = result.ConvertedCode
+                    Dim outFile = String.Concat(inFile.AsSpan(0, inFile.Length - 2), "vb")
+                    File.Delete(inFile)
+                    deletedFiles.Add(New Microsoft.Build.Utilities.TaskItem(inFile))
+                    generatedFiles.Add(New Microsoft.Build.Utilities.TaskItem(outFile))
+                Catch ex As Exception
+                    Log.LogMessage($"Failed to convert file '{inFile}', see exception below.")
+                    Log.LogErrorFromException(ex, True, True, Nothing)
+                    LogLoadedAssemblies()
+                    Return False
+                End Try
             Next
 
             _DeletedCodeFiles = deletedFiles.ToArray
@@ -86,6 +93,15 @@ Namespace Tasks
 
             Return True
         End Function
+
+        Private Sub LogLoadedAssemblies()
+            Dim loadedAsms = From asm In AppDomain.CurrentDomain.GetAssemblies
+                             Select $"  Assembly '{asm.FullName}' at '{asm.Location}'"
+
+            Log.LogMessage("Loaded assemblies")
+            Log.LogMessage(String.Join(Environment.NewLine, loadedAsms))
+            Log.LogMessage("End Loaded assemblies")
+        End Sub
 
         Private Function PrintTaskItem(compileCodeFiles() As ITaskItem) As String
             Return String.Join(";"c, From itm In compileCodeFiles Select itm.ItemSpec)
