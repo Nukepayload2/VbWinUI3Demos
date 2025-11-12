1e08dc9be1469957c68ef471fe49e0b91d78a2fd
Nukepayload2 <nukepayload2@outlook.com>
2025-11-13 00:11:09 +0800

    Add WinUI target pack

---
+++ WinUIVbTargetPack/Placeholder.vb
@@ -0,0 +1,7 @@
+' This is a placeholder file for the tool package.
+' This package only provides build assets and should not compile any runtime code.
+
+Public NotInheritable Class Placeholder
+    Private Sub New()
+    End Sub
+End Class
